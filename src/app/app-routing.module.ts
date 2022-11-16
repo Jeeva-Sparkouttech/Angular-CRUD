488122c71ac3cd6a86180afccd0258606cde1b00
Jeeva <jeevabaskaran328@gmail.com>
2022-11-16 18:47:34 +0530

    Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateDataComponent } from './create-data/create-data.component';
 import { HomeComponent } from './home/home.component';
 import { UpdateDataComponent } from './update-data/update-data.component';
+import { ValidationUsingServiceComponent } from './validation-using-service/validation-using-service.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
 {path:'home',component:HomeComponent},
 {path:'create-data',component:CreateDataComponent},
 {path:'update-data/:id',component:UpdateDataComponent},
+{path : 'validation-service',component : ValidationUsingServiceComponent}
 ]
 
 @NgModule({
@@ -16,3 +18,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ HomeComponent,
+  CreateDataComponent,
+  UpdateDataComponent,
+  ValidationUsingServiceComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {HttpClientModule} from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CreateDataComponent } from './create-data/create-data.component';
-import { UpdateDataComponent } from './update-data/update-data.component';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
-import { ValidationUsingServiceComponent } from './validation-using-service/validation-using-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    CreateDataComponent,
-    UpdateDataComponent,
-    ValidationUsingServiceComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
@@ -29,3 +22,4 @@ import { ValidationUsingServiceComponent } from './validation-using-service/vali
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
